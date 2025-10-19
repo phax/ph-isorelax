d7cd8bbc9b048cc67e131aa585c10edb0f3f2c21
Philip Helger <philip@helger.com>
2025-10-19 20:44:14 +0200

    Fixed javadoc errors

--- src/main/java/org/iso_relax/verifier/VerifierFactory.java
+++ src/main/java/org/iso_relax/verifier/VerifierFactory.java
@@ -42,11 +42,11 @@ public abstract class VerifierFactory
   private EntityResolver m_aResolver;
 
   /**
-   * parses a schema at the specified location and returns a Verifier object
-   * that validates documents by using that schema.
+   * parses a schema at the specified location and returns a Verifier object that validates
+   * documents by using that schema.
    * <p>
-   * Some of XML parsers accepts filenames as well as URLs, while others reject
-   * them. Therefore, to parse a file as a schema, you should use a File object.
+   * Some of XML parsers accepts filenames as well as URLs, while others reject them. Therefore, to
+   * parse a file as a schema, you should use a File object.
    *
    * @param uri
    *        URI of a schema file
@@ -57,47 +57,45 @@ public abstract class VerifierFactory
   }
 
   /**
-   * parses a schema from the specified file and returns a Verifier object that
-   * validates documents by using that schema.
+   * parses a schema from the specified file and returns a Verifier object that validates documents
+   * by using that schema.
    *
    * @param file
    *        File of a schema file
    */
   public Verifier newVerifier (final File file) throws VerifierConfigurationException, SAXException, IOException
   {
-
     return compileSchema (file).newVerifier ();
   }
 
   /**
-   * parses a schema from the specified InputStream and returns a Verifier
-   * object that validates documents by using that schema.
+   * parses a schema from the specified InputStream and returns a Verifier object that validates
+   * documents by using that schema.
    */
   public Verifier newVerifier (final InputStream stream) throws VerifierConfigurationException,
                                                          SAXException,
                                                          IOException
   {
-
     return compileSchema (stream, null).newVerifier ();
   }
 
   /**
-   * parses a schema from the specified InputStream and returns a Verifier
-   * object that validates documents by using that schema.
+   * parses a schema from the specified InputStream and returns a Verifier object that validates
+   * documents by using that schema.
    *
    * @param systemId
    *        System ID of this stream.
    */
-  public Verifier newVerifier (final InputStream stream,
-                               final String systemId) throws VerifierConfigurationException, SAXException, IOException
+  public Verifier newVerifier (final InputStream stream, final String systemId) throws VerifierConfigurationException,
+                                                                                SAXException,
+                                                                                IOException
   {
-
     return compileSchema (stream, systemId).newVerifier ();
   }
 
   /**
-   * parses a schema from the specified InputSource and returns a Verifier
-   * object that validates documents by using that schema.
+   * parses a schema from the specified InputSource and returns a Verifier object that validates
+   * documents by using that schema.
    *
    * @param source
    *        InputSource of a schema file
@@ -106,40 +104,35 @@ public abstract class VerifierFactory
                                                          SAXException,
                                                          IOException
   {
-
     return compileSchema (source).newVerifier ();
   }
 
   /**
-   * processes a schema into a Schema object, which is a compiled representation
-   * of a schema. The obtained schema object can then be used concurrently
-   * across multiple threads.
+   * processes a schema into a Schema object, which is a compiled representation of a schema. The
+   * obtained schema object can then be used concurrently across multiple threads.
    */
   public abstract Schema compileSchema (InputSource is) throws VerifierConfigurationException,
                                                         SAXException,
                                                         IOException;
 
   /**
-   * processes a schema into a Schema object, which is a compiled representation
-   * of a schema. The obtained schema object can then be used concurrently
-   * across multiple threads.
+   * processes a schema into a Schema object, which is a compiled representation of a schema. The
+   * obtained schema object can then be used concurrently across multiple threads.
    * <p>
-   * Some of XML parsers accepts filenames as well as URLs, while others reject
-   * them. Therefore, to parse a file as a schema, you should use a File object.
+   * Some of XML parsers accepts filenames as well as URLs, while others reject them. Therefore, to
+   * parse a file as a schema, you should use a File object.
    *
    * @param url
    *        A source url of a schema file to be compiled.
    */
   public Schema compileSchema (final String url) throws VerifierConfigurationException, SAXException, IOException
   {
-
     return compileSchema (new InputSource (url));
   }
 
   /**
-   * processes a schema into a Schema object, which is a compiled representation
-   * of a schema. The obtained schema object can then be used concurrently
-   * across multiple threads.
+   * processes a schema into a Schema object, which is a compiled representation of a schema. The
+   * obtained schema object can then be used concurrently across multiple threads.
    *
    * @param stream
    *        A stream object that holds a schema.
@@ -148,38 +141,34 @@ public abstract class VerifierFactory
                                                          SAXException,
                                                          IOException
   {
-
     return compileSchema (stream, null);
   }
 
   /**
-   * processes a schema into a Schema object, which is a compiled representation
-   * of a schema. The obtained schema object can then be used concurrently
-   * across multiple threads.
+   * processes a schema into a Schema object, which is a compiled representation of a schema. The
+   * obtained schema object can then be used concurrently across multiple threads.
    *
    * @param systemId
    *        The system Id of this input stream.
    */
-  public Schema compileSchema (final InputStream stream,
-                               final String systemId) throws VerifierConfigurationException, SAXException, IOException
+  public Schema compileSchema (final InputStream stream, final String systemId) throws VerifierConfigurationException,
+                                                                                SAXException,
+                                                                                IOException
   {
-
     final InputSource is = new InputSource (stream);
     is.setSystemId (systemId);
     return compileSchema (is);
   }
 
   /**
-   * processes a schema into a Schema object, which is a compiled representation
-   * of a schema. The obtained schema object can then be used concurrently
-   * across multiple threads.
+   * processes a schema into a Schema object, which is a compiled representation of a schema. The
+   * obtained schema object can then be used concurrently across multiple threads.
    *
    * @param f
    *        A schema file to be compiled
    */
   public Schema compileSchema (final File f) throws VerifierConfigurationException, SAXException, IOException
   {
-
     String uri = "file:" + f.getAbsolutePath ();
     if (File.separatorChar == '\\')
     {
@@ -197,7 +186,6 @@ public abstract class VerifierFactory
   @SuppressWarnings ("deprecation")
   public boolean isFeature (final String feature) throws SAXNotRecognizedException
   {
-
     if (Verifier.FEATURE_HANDLER.equals (feature) || Verifier.FEATURE_FILTER.equals (feature))
       return true;
 
@@ -243,8 +231,8 @@ public abstract class VerifierFactory
   }
 
   /**
-   * Sets an EntityResolver This entity resolver is used to resolve entities
-   * encountered while parsing a schema.
+   * Sets an EntityResolver This entity resolver is used to resolve entities encountered while
+   * parsing a schema.
    */
   public void setEntityResolver (final EntityResolver _resolver)
   {
@@ -252,8 +240,7 @@ public abstract class VerifierFactory
   }
 
   /**
-   * Gets the current entity resolver, which was set by the
-   * <code>SetEntityResolver</code> method.
+   * Gets the current entity resolver, which was set by the <code>SetEntityResolver</code> method.
    */
   public EntityResolver getEntityResolver ()
   {
@@ -270,45 +257,42 @@ public abstract class VerifierFactory
   }
 
   /**
-   * Creates a new instance of a VerifierFactory for the specified schema
-   * language.
+   * Creates a new instance of a VerifierFactory for the specified schema language.
    *
    * @param language
    *        URI that specifies the schema language.
    *        <p>
-   *        It is preferable to use the namespace URI of the schema language to
-   *        designate the schema language. For example,
-   *        <table summary="possibilities">
-   *        <thead>
+   *        It is preferable to use the namespace URI of the schema language to designate the schema
+   *        language. For example,
+   *        <table>
+   *        <caption>possibilities</caption><thead>
    *        <tr>
    *        <th>URI</th>
    *        <th>language</th>
    *        </tr>
    *        </thead><tbody>
    *        <tr>
-   *        <td><tt>http://relaxng.org/ns/structure/0.9</tt></td>
-   *        <td><a href="http://www.oasis-open.org/committees/relax-ng/"> RELAX
-   *        NG </a></td>
+   *        <td><code>http://relaxng.org/ns/structure/0.9</code></td>
+   *        <td><a href="http://www.oasis-open.org/committees/relax-ng/"> RELAX NG </a></td>
    *        </tr>
    *        <tr>
-   *        <td><tt>http://www.xml.gr.jp/xmlns/relaxCore</tt></td>
+   *        <td><code>http://www.xml.gr.jp/xmlns/relaxCore</code></td>
    *        <td><a href="http://www.xml.gr.jp/relax"> RELAX Core </a></td>
    *        </tr>
    *        <tr>
-   *        <td><tt>http://www.xml.gr.jp/xmlns/relaxNamespace</tt></td>
+   *        <td><code>http://www.xml.gr.jp/xmlns/relaxNamespace</code></td>
    *        <td><a href="http://www.xml.gr.jp/relax"> RELAX Namespace </a></td>
    *        </tr>
    *        <tr>
-   *        <td><tt>http://www.thaiopensource.com/trex</tt></td>
+   *        <td><code>http://www.thaiopensource.com/trex</code></td>
    *        <td><a href="http://www.thaiopensource.com/trex"> TREX </a></td>
    *        </tr>
    *        <tr>
-   *        <td><tt>http://www.w3.org/2001/XMLSchema</tt></td>
-   *        <td><a href="http://www.w3.org/TR/xmlschema-1"> W3C XML Schema </a>
-   *        </td>
+   *        <td><code>http://www.w3.org/2001/XMLSchema</code></td>
+   *        <td><a href="http://www.w3.org/TR/xmlschema-1"> W3C XML Schema </a></td>
    *        </tr>
    *        <tr>
-   *        <td><tt>http://www.w3.org/XML/1998/namespace</tt></td>
+   *        <td><code>http://www.w3.org/XML/1998/namespace</code></td>
    *        <td><a href="http://www.w3.org/TR/REC-xml"> XML DTD </a></td>
    *        </tr>
    *        </tbody>
@@ -319,8 +303,8 @@ public abstract class VerifierFactory
    * @exception VerifierConfigurationException
    *            if no implementation is available for the specified language.
    */
-  public static VerifierFactory newInstance (final String language,
-                                             final ClassLoader classLoader) throws VerifierConfigurationException
+  public static VerifierFactory newInstance (final String language, final ClassLoader classLoader)
+                                                                                                   throws VerifierConfigurationException
   {
     for (final VerifierFactoryLoader loader : ServiceLoader.load (VerifierFactoryLoader.class, classLoader))
     {
